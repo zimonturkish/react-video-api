eec765695caa8f2921430d547897dc88354015fb
Simone Turco <simone.turco@charite.de>
2020-02-18 18:36:32 +0100

    creating api request in youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 const KEY = "AIzaSyAjAJVro_cBmek72z-Kw377ltMtrvJFhqs"; // uppercase because this specifically says this is a costant value
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
