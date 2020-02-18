c39be5429f0fa8e9296b376b26a66a386e08f90f
Simone Turco <simone.turco@charite.de>
2020-02-18 17:04:56 +0100

    adding onFormSubmit =>e.preventDefault()

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,11 +6,14 @@ class SearchBar extends React.Component {
   onInputChange = event => {
     this.setState({ term: event.target.value });
   };
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit="this.onFormSubmit" className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
