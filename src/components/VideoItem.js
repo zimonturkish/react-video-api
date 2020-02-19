024158d48c6ec73dfb3e0846d2513cf78cf1a0fb
Simone Turco <simone.turco@charite.de>
2020-02-19 16:27:03 +0100

    className from semantic ui for VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,9 +3,11 @@ import React from "react";
 const VideoItem = ({ video }) => {
   // props now contains the video property (video=item)
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title}
+    <div className="item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   ); // to print the titles of the videos on the screen
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({ videos }) => {
   const renderedList = videos.map(video => {
     return <VideoItem video={video} />; //video we are iterating over, in the map()
   });
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed diveded list">{renderedList}</div>;
 };
 
 export default VideoList;
