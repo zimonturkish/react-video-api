89ca8f651c1ea5e88a64b096fabbf6184bd1061f
Simone Turco <simone.turco@charite.de>
2020-02-19 17:23:03 +0100

    adding some css

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import React from "react";
 const VideoItem = ({ video }) => {
   // props now contains the video property (video=item)
   return (
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
