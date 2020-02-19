0133ee17007d1b1402d93361be38f19758866f8e
Simone Turco <simone.turco@charite.de>
2020-02-19 19:11:02 +0100

    key in VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />; //video we are iterating over, in the map()
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    ); //video we are iterating over, in the map()
   });
   return <div className="ui relaxed diveded list">{renderedList}</div>;
 };
