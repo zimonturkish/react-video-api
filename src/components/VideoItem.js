fd41c9967f63debb592aaf3319e841d40fcbfc00
Simone Turco <simone.turco@charite.de>
2020-02-19 15:39:03 +0100

    VideoItems print titles on the screen

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const VideoItem = props => {
+const VideoItem = ({ video }) => {
   // props now contains the video property (video=item)
-  return <div>VideoItem</div>;
+  return <div>{video.snippet.title}</div>; // to print the titles of the videos on the screen
 };
 
 export default VideoItem;
