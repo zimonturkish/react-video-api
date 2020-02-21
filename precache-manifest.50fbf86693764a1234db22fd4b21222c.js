bc9328fcb6fa344468a519ecbf5f9ea0999fdee6
Turco <turcos@charite.de>
2020-02-21 11:25:32 +0100

    Updates

---
+++ precache-manifest.50fbf86693764a1234db22fd4b21222c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f63ffdcfcd9e2e4580328fa65d017a",
+    "url": "/react-video-api/index.html"
+  },
+  {
+    "revision": "12c09c10b848f89e9da4",
+    "url": "/react-video-api/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "58734015ed2c5cd37940",
+    "url": "/react-video-api/static/js/2.39b415eb.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/react-video-api/static/js/2.39b415eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c09c10b848f89e9da4",
+    "url": "/react-video-api/static/js/main.06c72ef8.chunk.js"
+  },
+  {
+    "revision": "6da3a4983ed34b8ad220",
+    "url": "/react-video-api/static/js/runtime-main.ac5675d1.js"
+  }
+]);
